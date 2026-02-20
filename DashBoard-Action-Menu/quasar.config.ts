ce930c08aee3acfeae5d989971924d97e7b1fbbf
AndrAlcsndr <andralcsndr@gmail.com>
2026-02-20 18:52:08 -0300

    correções de pacote eslint

--- DashBoard-Action-Menu/quasar.config.ts
+++ DashBoard-Action-Menu/quasar.config.ts
@@ -64,17 +64,6 @@ export default defineConfig((/* ctx */) => {
       // viteVuePluginOptions: {},
 
       vitePlugins: [
-        [
-          'vite-plugin-checker',
-          {
-            vueTsc: true,
-            eslint: {
-              lintCommand: 'eslint -c ./eslint.config.js "./src*/**/*.{ts,js,mjs,cjs,vue}"',
-              useFlatConfig: true,
-            },
-          },
-          { server: false },
-        ],
       ],
     },
 
