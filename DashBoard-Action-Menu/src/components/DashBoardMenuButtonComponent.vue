59bf62a9cad1383429495477c3137e05dccb0e0b
AndrAlcsndr <andralcsndr@gmail.com>
2026-02-20 16:14:00 -0300

    criação de estrutura basica dos botoes

--- DashBoard-Action-Menu/src/components/DashBoardMenuButtonComponent.vue
+++ DashBoard-Action-Menu/src/components/DashBoardMenuButtonComponent.vue
@@ -3,14 +3,28 @@
     class="q-pl-md text-black"
     style="border-radius: 0% !important; box-shadow: unset; margin-bottom: 15px"
   >
-  <q-card-section class="row items-center q-pa-none">
-    <div>
-        
-    </div>
-  </q-card-section>  
-
+    <q-card-section class="row items-center q-pa-none">
+      <div></div>
+    </q-card-section>
   </q-card>
-  
 </template>
 
-<script lang="ts"></script>
+<script lang="ts">
+import { ref } from 'vue';
+
+const isAdmin = ref(true);
+
+let componentBtnsRegular = [
+  [
+    {
+      label: 'Incluir',
+      style: 'width: 160px',
+      icon: '',
+      action: function () {},
+      show: function () {
+        return isAdmin.value;
+      },
+    },
+  ],
+];
+</script>
