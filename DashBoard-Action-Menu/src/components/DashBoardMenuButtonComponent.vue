ce9948736f63cfa20adad7a2702a44a0a9a693c8
AndrAlcsndr <andralcsndr@gmail.com>
2026-02-20 16:07:20 -0300

    montagem de estrutura basica

---
+++ DashBoard-Action-Menu/src/components/DashBoardMenuButtonComponent.vue
@@ -0,0 +1,16 @@
+<template>
+  <q-card
+    class="q-pl-md text-black"
+    style="border-radius: 0% !important; box-shadow: unset; margin-bottom: 15px"
+  >
+  <q-card-section class="row items-center q-pa-none">
+    <div>
+        
+    </div>
+  </q-card-section>  
+
+  </q-card>
+  
+</template>
+
+<script lang="ts"></script>
