5659b0b034c7d22a90f990dd566efebd072747a3
AndrAlcsndr <andralcsndr@gmail.com>
2026-02-21 13:00:14 -0300

    implem. de rotas para as paginas

--- DashBoard-Action-Menu/src/router/routes.ts
+++ DashBoard-Action-Menu/src/router/routes.ts
@@ -9,24 +9,24 @@ const routes: RouteRecordRaw[] = [
   {
     path: '/inclusao',
     component: () => import('layouts/MainLayout.vue'),
-    children: [{ path: '', component: () => import('pages/IndexPage.vue') }],
+    children: [{ path: '', component: () => import('pages/Itens/InclusaoItem.vue') }],
   },
   {
     path: '/rascunho',
     component: () => import('layouts/MainLayout.vue'),
-    children: [{ path: '', component: () => import('pages/IndexPage.vue') }],
+    children: [{ path: '', component: () => import('pages/Itens/RascunhoItem.vue') }],
   },
   {
     path: '/relatorios',
     component: () => import('layouts/MainLayout.vue'),
-    children: [{ path: '', component: () => import('pages/IndexPage.vue') }],
+    children: [{ path: '', component: () => import('pages/Itens/RelatorioItem.vue') }],
   },
 
   // Always leave this as last one,
   // but you can also remove it
   {
     path: '/:catchAll(.*)*',
-    component: () => import('pages/ErrorNotFound.vue'),
+    component: () => import('pages/ErrorRoute/ErrorNotFound.vue'),
   },
 ];
 
